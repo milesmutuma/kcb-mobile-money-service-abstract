2f38d80a3b1e8dc00070f40f88861a59fe7f33ab
milesmutuma <mutumamiles@gmail.com>
2025-03-13 11:08:18 +0300

    added unimplemented comment

--- src/main/java/com/kcb/mobilemoney/service/SmsNotificationServiceImpl.java
+++ src/main/java/com/kcb/mobilemoney/service/SmsNotificationServiceImpl.java
@@ -1,7 +1,6 @@
 package com.kcb.mobilemoney.service;
 
 import com.kcb.mobilemoney.model.Transaction;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,10 +9,15 @@ public class SmsNotificationServiceImpl implements NotificationService {
     @Override
     public void sendTransactionNotification(Transaction transaction) {
         // send SMS notification
+        // UNIMPLEMENTED
+        // We can use Twilio or any other SMS service provider to send SMS
     }
 
     @Override
     public void sendFailureNotification(Transaction transaction, String errorMessage) {
         // send SMS Failure notification
+        // UNIMPLEMENTED
+
+        // We can use Twilio or any other SMS service provider to send SMS
     }
 }
